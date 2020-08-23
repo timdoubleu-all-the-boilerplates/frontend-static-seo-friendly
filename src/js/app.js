101ddc2342679566b2a9b4a934586b6b28a6c0ef
Tim <twilliams25@gmail.com>
2020-08-22 23:26:34 -0700

    refactored removing webpack and going with parcel for seo friendly multipage sites, non SPA sites

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 // App wide root JS, a place you can import your libraries, and write JS needed across the entire site
-import { saySomething } from '../util/utility';
+import { saySomething } from './lib/utility';
 import $ from 'jquery';
 
 console.log('App wide JS code, including libraries, like jQuery:');
@@ -11,5 +11,5 @@ $(document).ready(function() {
 
 const greeting = saySomething("Taco");
 let myH1 = document.createElement('h1');
-myH1.innerText = greeting
+myH1.innerText = greeting;
 document.body.appendChild(myH1);
